ce0b618ef0f0d1ac4c69951b5c99eb57abfbaaff
abrar-arian <abrarfaiyazarian@gmail.com>
2026-08-21 12:20:58 +0600

    explanatory comments added

--- src/timer.c
+++ src/timer.c
@@ -1,10 +1,15 @@
 #include "timer.h"
-
+//quation marks instead of <> mean look into our own project folder
 void InitTimer(Timer *t, float startingTime) {
     t->timeLeft = startingTime;
     t->frozen = 0;
 }
+//startingtime is the arguement passed to the function
+//whatever value is passed become the time to complete the game
 
 void UpdateTimer(Timer *t, float delta) {
     if (!t->frozen) t->timeLeft -= delta;
 }
+//delta= time passed since last frame
+//delta makes time accurate regardless of the timeframe
+//subtraction only happens when timer is not frozen 
